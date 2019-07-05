aa7a77066617d6ba3d5fd6d080a10f7be54c007d
njcorona <43733460+njcorona@users.noreply.github.com>
2019-07-05 17:45:35 -0400

    Created executable .R file from .Rmd
    Need executable R files to be able to run them automatically daily/weekly/monthly/etc.

---
+++ ElectricRazors/ElectricRazors.R
@@ -0,0 +1,180 @@
+#' ---
+#' title: "ElectricRazors"
+#' author: "NicolasCorona"
+#' date: "6/16/2019"
+#' output: html_document
+#' ---
+#' 
+## ----setup, include=FALSE------------------------------------------------
+knitr::opts_chunk$set(echo = TRUE)
+suppressWarnings(suppressMessages(library(tidyverse)))
+suppressWarnings(suppressMessages(library(httr)))
+suppressWarnings(suppressMessages(library(stringr)))
+suppressWarnings(suppressMessages(library(rvest)))
+suppressWarnings(suppressMessages(library(magrittr)))
+suppressWarnings(suppressMessages(library(future)))
+
+#' 
+## ------------------------------------------------------------------------
+date <- "6_23_"
+arg_p <- read_csv(date %>% paste("ElectricRazors_Prod_Arg.csv", sep = ""), col_names = TRUE)
+bra_p <- read_csv(date %>% paste("ElectricRazors_Prod_Bra.csv", sep = ""), col_names = TRUE)
+chi_p <- read_csv(date %>% paste("ElectricRazors_Prod_Chi.csv", sep = ""), col_names = TRUE)
+col_p <- read_csv(date %>% paste("ElectricRazors_Prod_Col.csv", sep = ""), col_names = TRUE)
+mex_p <- read_csv(date %>% paste("ElectricRazors_Prod_Mex.csv", sep = ""), col_names = TRUE)
+per_p <- read_csv(date %>% paste("ElectricRazors_Prod_Per.csv", sep = ""), col_names = TRUE)
+uru_p <- read_csv(date %>% paste("ElectricRazors_Prod_Uru.csv", sep = ""), col_names = TRUE)
+
+arg_s <- read_csv(date %>% paste("ElectricRazors_Sell_Arg.csv", sep = ""), col_names = TRUE)
+bra_s <- read_csv(date %>% paste("ElectricRazors_Sell_Bra.csv", sep = ""), col_names = TRUE)
+chi_s <- read_csv(date %>% paste("ElectricRazors_Sell_Chi.csv", sep = ""), col_names = TRUE)
+col_s <- read_csv(date %>% paste("ElectricRazors_Sell_Col.csv", sep = ""), col_names = TRUE)
+mex_s <- read_csv(date %>% paste("ElectricRazors_Sell_Mex.csv", sep = ""), col_names = TRUE)
+per_s <- read_csv(date %>% paste("ElectricRazors_Sell_Per.csv", sep = ""), col_names = TRUE)
+uru_s <- read_csv(date %>% paste("ElectricRazors_Sell_Uru.csv", sep = ""), col_names = TRUE)
+
+arg_p <- cbind(country = "ARG", arg_p)
+bra_p <- cbind(country = "BRA", bra_p)
+chi_p <- cbind(country = "CHI", chi_p)
+col_p <- cbind(country = "COL", col_p)
+mex_p <- cbind(country = "MEX", mex_p)
+per_p <- cbind(country = "PER", per_p)
+uru_p <- cbind(country = "URU", uru_p)
+
+arg_s <- cbind(country = "ARG", arg_s)
+bra_s <- cbind(country = "BRA", bra_s)
+chi_s <- cbind(country = "CHI", chi_s)
+col_s <- cbind(country = "COL", col_s)
+mex_s <- cbind(country = "MEX", mex_s)
+per_s <- cbind(country = "PER", per_s)
+uru_s <- cbind(country = "URU", uru_s)
+
+arg_p$country <- as.character(arg_p$country)
+bra_p$country <- as.character(bra_p$country)
+chi_p$country <- as.character(chi_p$country)
+col_p$country <- as.character(col_p$country)
+mex_p$country <- as.character(mex_p$country)
+per_p$country <- as.character(per_p$country)
+uru_p$country <- as.character(uru_p$country)
+
+arg_s$country <- as.character(arg_s$country)
+bra_s$country <- as.character(bra_s$country)
+chi_s$country <- as.character(chi_s$country)
+col_s$country <- as.character(col_s$country)
+mex_s$country <- as.character(mex_s$country)
+per_s$country <- as.character(per_s$country)
+uru_s$country <- as.character(uru_s$country)
+
+arg_p$num_installments <- as.character(arg_p$num_installments)
+bra_p$num_installments <- as.character(bra_p$num_installments)
+chi_p$num_installments <- as.character(chi_p$num_installments)
+col_p$num_installments <- as.character(col_p$num_installments)
+mex_p$num_installments <- as.character(mex_p$num_installments)
+per_p$num_installments <- as.character(per_p$num_installments)
+uru_p$num_installments <- as.character(uru_p$num_installments)
+
+arg_p$amt_installments <- as.character(arg_p$amt_installments)
+bra_p$amt_installments <- as.character(bra_p$amt_installments)
+chi_p$amt_installments <- as.character(chi_p$amt_installments)
+col_p$amt_installments <- as.character(col_p$amt_installments)
+mex_p$amt_installments <- as.character(mex_p$amt_installments)
+per_p$amt_installments <- as.character(per_p$amt_installments)
+uru_p$amt_installments <- as.character(uru_p$amt_installments)
+
+prod <- bind_rows(arg_p, bra_p, chi_p, col_p, mex_p, per_p, uru_p)
+sell <- bind_rows(arg_s, bra_s, chi_s, col_s, mex_s, per_s, uru_s)
+
+# Names are reversed.  Here they are unreversed.
+stored_units_timeframe_of_amt_sold <- sell$units_timeframe_of_amt_sold
+sell$units_timeframe_of_amt_sold <- sell$timeframe_of_amt_sold
+sell$timeframe_of_amt_sold <- stored_units_timeframe_of_amt_sold
+
+# Combining plural/singuler/Spanish/Portuguese words.
+sell$units_of_time_operating[which(sell$units_of_time_operating == "ano")] <- "años"
+sell$units_of_time_operating[which(sell$units_of_time_operating == "anos")] <- "años"
+sell$units_of_time_operating[which(sell$units_of_time_operating == "año")] <- "años"
+
+sell$units_of_time_operating[which(sell$units_of_time_operating == "mês")] <- "meses"
+sell$units_of_time_operating[which(sell$units_of_time_operating == "mes")] <- "meses"
+
+sell$units_timeframe_of_amt_sold[which(sell$units_timeframe_of_amt_sold == "anos")] <- "años"
+sell$units_timeframe_of_amt_sold[which(sell$units_timeframe_of_amt_sold == "mes")] <- "meses"
+
+prod$arrival_time[which(prod$arrival_time == "Envio")] <- "Envío"
+prod$arrival_time[which(prod$arrival_time == "Envio para todo o país")] <- "Envío a todo el país"
+prod$arrival_time[which(prod$arrival_time == "Envío a nivel nacional")] <- "Envío a todo el país"
+prod$arrival_time[which(prod$arrival_time == "Entrega a combinar com o vendedor")] <- "Entrega a acordar con el vendedor"
+
+prod$shipping[which(prod$shipping == "Frete grátis")] <- "Envío gratis"
+prod$shipping[which(prod$shipping == "Envío gratis a nivel nacional")] <- "Envío gratis a todo el país"
+
+prod$free_return[which(prod$free_return == "Devolução grátis")] <- "Devolución gratis"
+
+prod$free_return_info[which(prod$free_return_info == "Você tem 10 dias a partir do recebimento")] <- "Tenés 10 días desde que lo recibís"
+prod$free_return_info[which(prod$free_return_info == "Tienes 10 días desde que lo recibes")] <- "Tenés 10 días desde que lo recibís"
+
+# Converting to integers.
+units_of_time_operating_values <- sell$units_of_time_operating[!duplicated(sell$units_of_time_operating)]
+units_of_time_operating_values <- units_of_time_operating_values[!is.na(units_of_time_operating_values)]
+
+sell$units_of_time_operating <- sapply(sell$units_of_time_operating, function(x) { return ( if (is.na(x)) { NA } else { which(x == units_of_time_operating_values) }) })
+
+# These were commented out.  Why?
+units_timeframe_of_amt_sold_values <- sell$units_timeframe_of_amt_sold[!duplicated(sell$units_timeframe_of_amt_sold)]
+units_timeframe_of_amt_sold_values <- units_timeframe_of_amt_sold_values[!is.na(units_timeframe_of_amt_sold_values)]
+
+sell$units_timeframe_of_amt_sold <- sapply(sell$units_timeframe_of_amt_sold, function(x) { return ( if (is.na(x)) { NA } else { which(x == units_of_time_operating_values) }) })
+
+arrival_time_values <- prod$arrival_time[!duplicated(prod$arrival_time)]
+arrival_time_values <- arrival_time_values[!is.na(arrival_time_values)]
+
+prod$arrival_time <- sapply(prod$arrival_time, function(x) { return ( if (is.na(x)) { NA } else { which(x == arrival_time_values) }) })
+
+shipping_values <- prod$shipping[!duplicated(prod$shipping)]
+shipping_values <- shipping_values[!is.na(shipping_values)]
+
+prod$shipping <- sapply(prod$shipping, function(x) { return ( if (is.na(x)) { NA } else { which(x == shipping_values) }) })
+
+free_return_values <- prod$free_return[!duplicated(prod$free_return)]
+free_return_values <- free_return_values[!is.na(free_return_values)]
+
+prod$free_return <- sapply(prod$free_return, function(x) { return ( if (is.na(x)) { NA } else { which(x == free_return_values) }) })
+
+free_return_info_values <- prod$free_return_info[!duplicated(prod$free_return_info)]
+free_return_info_values <- free_return_info_values[!is.na(free_return_info_values)]
+
+prod$free_return_info <- sapply(prod$free_return_info, function(x) { return ( if (is.na(x)) { NA } else { which(x == free_return_info_values) }) })
+
+# Combining characteristics with different names.
+prod$`Línea`[which(prod$country == "BRA")] <- prod$Linha[which(prod$country == "BRA")]
+prod$`Tipo de alimentación`[which(prod$country == "BRA")] <- prod$`Tipo de alimentação`[which(prod$country == "BRA")]
+prod$`Tipos de cabezales`[which(prod$country == "BRA")] <- prod$`Tipos de cabeças`[which(prod$country == "BRA")]
+prod$`Tiempo de funcionamiento`[which(prod$country == "BRA")] <- prod$`Tempo de funcionamento`[which(prod$country == "BRA")]
+
+prod$Linha <- NULL
+prod$`Tipo de alimentação` <- NULL
+prod$`Tipos de cabeças` <- NULL
+prod$`Tempo de funcionamento`<- NULL
+
+write_csv(prod, date %>% paste("ElectricRazors_Prod.csv", sep = ""))
+write_csv(sell, date %>% paste("ElectricRazors_Sell.csv", sep = ""))
+
+# I may want to remove this particular bit of cleaning until I bring all of the countries together.
+list <- list(units_of_time_operating_values, units_timeframe_of_amt_sold_values, arrival_time_values, shipping_values, free_return_values, free_return_info_values)
+max <- max( sapply(list, function(x) { length(x) }) )
+list <- lapply(list, function(x) {
+  if (length(x) < max) {
+    orig_l <- length(x)
+    x <- rep(x, length.out = max)
+    x[c((orig_l + 1):max)] <- "" # As opposed to " ".
+    return (x)
+  }
+  return (x)
+  
+})
+
+data_legend <- tibble(value = c(1:max), units_of_time_operating = list[[1]], units_timeframe_of_amt_sold = list[[1]], arrival_time = list[[3]], shipping = list[[4]], free_return = list[[5]], free_return_info = list[[6]])
+write_csv(data_legend, date %>% paste("ElectricRazors_Legend.csv", sep = ""))
+
+
+#' 
