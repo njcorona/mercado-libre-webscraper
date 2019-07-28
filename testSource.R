a4ac688dde2f04db4128736e78c13564473c7bda
njcorona <43733460+njcorona@users.noreply.github.com>
2019-07-28 19:24:01 -0400

    Updates

--- testSource.R
+++ testSource.R
@@ -12,3 +12,6 @@ tryCatch({
   print(Sys.time())
   Sys.sleep(120)
 })
+
+# This never timed out.  Even when being sourced.  I do not understand why I was running into timeout errors
+# on my other PC.
