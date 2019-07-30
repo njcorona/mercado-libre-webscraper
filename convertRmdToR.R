76177ad0ce5358a9a3d4475ac6be4581c36a8b13
njcorona <43733460+njcorona@users.noreply.github.com>
2019-07-29 20:05:19 -0400

    Update convertRmdToR.R
    Allows for updating filepath User name.

--- convertRmdToR.R
+++ convertRmdToR.R
@@ -23,3 +23,24 @@ updateFiles <- function(x) {
 }
 
 sapply(files, updateFiles)
+
+# Set working directory
+files <- c("iPhone8PlusCases", "iPhone8Cases", "ElectricRazors", "BTHeadphones", "BluetoothSpeakers", "MotorcycleHelmets")
+
+updateFiles <- function(x) {
+  filename <- x
+  top <- "C:/Users/njcor/Documents/GitHub/mercado-libre/" %>% paste(filename, sep = "") %>% paste("/", sep = "") %>% paste(filename, sep = "") %>% paste("_", sep = "")
+  
+  vec <- c("Arg", "Bra", "Chi", "Col", "Mex", "Per", "Uru")
+  
+  sapply(vec, function(x) {
+    Rfile <- top %>% paste(x, sep = "") %>% paste(".R", sep = "")
+    print(Rfile)
+    text <- readLines(Rfile)
+    text[17] <- str_replace(text[17], "Corona-Velez", "njcor")
+    writeLines(text, Rfile)
+  })
+  
+}
+
+sapply(files, updateFiles)
